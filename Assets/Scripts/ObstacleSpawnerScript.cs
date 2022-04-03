3771476abc3002cc0a070fd21dd900d5dafeca17
nof <daniil@generalarcade.com>
2022-04-04 02:29:23 +0400

    rework obstacles

--- Assets/Scripts/ObstacleSpawnerScript.cs
+++ Assets/Scripts/ObstacleSpawnerScript.cs
@@ -7,22 +7,37 @@ public class ObstacleSpawnerScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject obstacle;
+    [SerializeField]
+    private Sprite[] smallObstacleSprites;
+    [SerializeField]
+    private Sprite[] mediumObstacleSprites;
+    [SerializeField]
+    private Sprite[] bigObstacleSprites;
 
     private List<GameObject> _spawnedObstacles = new List<GameObject>();
     private float _delayModifier;
-    private Coroutine _spawnerCoroutine;
+    private Coroutine[] _spawnerCoroutines = new Coroutine[3];
+
+    private ObstacleType[] ObstaclesDescription;
 
     void Awake()
     {
-        for (int i = 0; i < 4; i++)
+        ObstaclesDescription = new ObstacleType[]
+        {
+            new ObstacleType(2, 5, 0.225f, 0.225f, 1, 5, smallObstacleSprites),
+            new ObstacleType(5, 5, 0.25f, 0.25f, 1, 2, mediumObstacleSprites),
+            new ObstacleType(10, 10, 0.225f, 0.225f, 1, 1, bigObstacleSprites),
+        };
+        for (int i = 0; i < 10; i++)
             _spawnedObstacles.Add(InstantiateObstacle());
         _delayModifier = 1f;
     }
 
     void Update()
     {
-        if (Time.timeScale != 0 && GameState.instance.GetSettings().isObstaclesEnabled && _spawnerCoroutine is null)
-            _spawnerCoroutine = StartCoroutine(SpawnObstaclesCoroutine());
+        if (Time.timeScale != 0 && GameState.instance.GetSettings().isObstaclesEnabled && _spawnerCoroutines.Any( x => x is null))
+            for (var i = 0; i < ObstaclesDescription.Length; i++)
+                _spawnerCoroutines[i] = StartCoroutine(SpawnObstaclesCoroutine(i));
     }
 
     private GameObject InstantiateObstacle()
@@ -37,18 +52,21 @@ public class ObstacleSpawnerScript : MonoBehaviour
         _delayModifier *= GameState.instance.GetSettings().obstaclesDelayModifier;
     }
 
-    private IEnumerator SpawnObstaclesCoroutine()
+    private IEnumerator SpawnObstaclesCoroutine(int index)
     {
-        yield return new WaitForSeconds(GameState.instance.GetSettings().obstaclesInitSpawnDelay * _delayModifier);
-        for (int i = 0; i < GameState.instance.GetSettings().obstaclesSpawnAmount; i++)
-            SpawnObstacle();
+        var obstacleType = ObstaclesDescription[index];
+        var timeToWait = Random.Range(obstacleType.MinDelay, obstacleType.MaxDelay);
+        var numberToSpawn = Random.Range(obstacleType.MinCount, obstacleType.MaxCount);
+        yield return new WaitForSeconds(timeToWait * _delayModifier);
+        for (int i = 0; i < numberToSpawn; i++)
+            SpawnObstacle(obstacleType);
         if (GameState.instance.GetSettings().isObstaclesEnabled)
-            _spawnerCoroutine = StartCoroutine(SpawnObstaclesCoroutine());
+            _spawnerCoroutines[index] = StartCoroutine(SpawnObstaclesCoroutine(index));
         else
-            _spawnerCoroutine = null;
+            _spawnerCoroutines[index] = null;
     }
 
-    private void SpawnObstacle()
+    private void SpawnObstacle(ObstacleType obstacleType)
     {
         var freeObstacle = _spawnedObstacles.FirstOrDefault(x => x.activeSelf == false);
         if (freeObstacle is null)
@@ -56,9 +74,35 @@ public class ObstacleSpawnerScript : MonoBehaviour
             freeObstacle = InstantiateObstacle();
             _spawnedObstacles.Add(freeObstacle);
         }
-        freeObstacle.transform.localScale = new Vector3(GameState.instance.GetSettings().obstaclesWidth, GameState.instance.GetSettings().obstaclesHeight, 1);
-        var width = freeObstacle.GetComponent<BoxCollider2D>().size.x * freeObstacle.transform.localScale.x / 2;
+        freeObstacle.transform.localScale = new Vector3(obstacleType.Width, obstacleType.Height, 1);
+        var spriteIndex = Random.Range(0, obstacleType.Sprites.Length - 1);
+        var boxcollider = freeObstacle.GetComponent<BoxCollider2D>();
+        boxcollider.size = obstacleType.Sprites[spriteIndex].bounds.size;
+        var width = boxcollider.size.x * freeObstacle.transform.localScale.x / 2;
         freeObstacle.transform.position = new Vector3(Random.Range(GameState.instance.GetRightWall().HighestCorner - width, GameState.instance.GetLeftWall().HighestCorner + width), 5.45f, 0);
+        freeObstacle.GetComponent<SpriteRenderer>().sprite = obstacleType.Sprites[spriteIndex];
         freeObstacle.SetActive(true);
+    }
+}
+
+public class ObstacleType
+{
+    public float MinDelay { get; set; }
+    public float MaxDelay { get; set; }
+    public float Width { get; set; }
+    public float Height { get; set; }
+    public int MinCount { get; set; }
+    public int MaxCount { get; set; }
+    public Sprite[] Sprites { get; set; }
+
+    public ObstacleType(float minDelay, float maxDelay, float width, float height, int minCount, int maxCount, Sprite[] sprites)
+    {
+        MinDelay = minDelay;
+        MaxDelay = maxDelay;
+        Width = width;
+        Height = height;
+        MinCount = minCount;
+        MaxCount = maxCount;
+        Sprites = sprites;
     }
 }
