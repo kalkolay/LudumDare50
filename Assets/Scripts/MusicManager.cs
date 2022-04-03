725bc95ade09da3a755b1811b3b195d0afd7a027
DESKTOP-BC722G9\ga <pavel@generalarcade.com>
2022-04-04 02:59:02 +0400

    Added music in game

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    AudioSource source;
+    AudioClip track;
+    float music_volume_scale = 0.1f;
+    bool flag_music_loop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(source.isPlaying && flag_music_loop)
+        {
+            PlayGameMusic(track);
+        }
+    }
+
+    public void PlayGameMusic(AudioClip _track)
+    {
+        source.PlayOneShot(_track);
+    }
+}
